f903849796cad20a551f8d3c55e36dcbf817fd2a
mandragora-hub <royer01gomez@gmail.comn>
2023-04-30 15:26:14 -0400

    Create Logo component

---
+++ components/Logo.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const app = useAppConfig();
+</script>
+>
+<template>
+  <NuxtLink
+    tag="a"
+    class="mr-3 flex-none overflow-hidden md:w-auto text-md font-bold text-gray-900 dark:text-gray-200"
+    to="/"
+    :class="$attrs.class"
+  >
+    <span class="sr-only">home</span>
+    <span class="flex items-center">
+      <Icon
+        name="simple-icons:nuxtdotjs"
+        class="inline-block mr-2 text-lg text-primary-500"
+      />
+      {{ app.name }}
+    </span>
+  </NuxtLink>
+</template>
