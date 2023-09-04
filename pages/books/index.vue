78e88c9a4a98053ce267b6ab074a9a977498bf27
mandragora-hub <royer01gomez@gmail.comn>
2023-09-03 21:57:22 -0400

    📙 Update book index pages

--- pages/books/index.vue
+++ pages/books/index.vue
@@ -1,11 +1,41 @@
 <script setup lang="ts">
+import type { Book } from "types";
 
+const { t } = useI18n();
+
+const el = ref<HTMLElement | null>(null);
+useInfiniteScroll(el, () => loadMore());
+
+const books = ref<Book[] | undefined>();
+const pages = ref(1);
+const initialData = await searchBooks("", pages.value, 5);
+books.value = initialData.data;
+
+const loadMore = async () => {
+  if (initialData.meta.totalPages <= pages.value) return;
+
+  pages.value++;
+  const result = await searchBooks("", pages.value, 5);
+  books.value?.push(...result.data);
+};
 </script>
 <template>
   <Container>
-    <h1 class="text-4xl font-bold">
-      Welcome to the <span class="text-blue-400">book page</span>
-    </h1>
-    <AppAlert>This is an auto-imported component</AppAlert>
+    <header class="mb-4">
+      <h1 class="text-4xl font-bold">
+        {{ t("pages.bookIndex.header") }}
+        <span role="img" alt="hello"> 📖️ </span>
+      </h1>
+      <p class="text-lg">
+        {{ t("pages.bookIndex.subtitle") }}
+      </p>
+    </header>
+    <div
+      v-if="initialData.meta.count"
+      ref="el"
+      class="grid grid-cols-2 gap-4 md:grid-cols-4 xl:grid-cols-5"
+    >
+      <MediaBook v-for="i in books" :key="i._id" :item="i" />
+    </div>
   </Container>
 </template>
