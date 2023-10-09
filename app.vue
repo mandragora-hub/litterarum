ac68879ea5a269b3edadd342a7d74cb0ce45d0a2
mandragora-hub <royer01gomez@gmail.comn>
2023-10-08 20:25:11 -0400

    📤 use default attributes in loading indicator

--- app.vue
+++ app.vue
@@ -36,7 +36,7 @@ useHead({
       class="antialiased duration-300 transition-colors text-gray-800 dark:text-gray-200 bg-white dark:bg-gray-900"
     >
       <NuxtLayout>
-        <NuxtLoadingIndicator :height="5" :duration="3000" :throttle="400" />
+        <NuxtLoadingIndicator />
         <NuxtPage />
       </NuxtLayout>
     </Body>
