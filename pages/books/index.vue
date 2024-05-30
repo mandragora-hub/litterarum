8e3f32b7816709776aa23d2d8913409d43a24914
mandragora-hub <royer01gomez@gmail.comn>
2024-05-30 15:03:10 -0400

    fix: update infinite scroll

--- pages/books/index.vue
+++ pages/books/index.vue
@@ -4,19 +4,24 @@ import type { Book } from "~/types";
 const { t } = useI18n();
 const app = useAppConfig();
 
-const el = ref<HTMLElement | null>(null);
-useInfiniteScroll(el, () => loadMore());
+// it should not be used here. Change this when create services folder.
+const { loading: globalLoading } = useGlobalFetchLoading(); 
+
+const el = ref<HTMLElement | null>(null); // remote this line.
+onMounted(() => {
+  useInfiniteScroll(window, () => loadMore());
+});
 
 const books = ref<Book[] | undefined>();
 const pages = ref(1);
-const { data: initialData } = await searchBooks("", pages.value, 5);
+const { data: initialData } = await searchBooks("", pages.value, 10);
 books.value = initialData.value?.data;
 
 const loadMore = async () => {
   if (initialData.value!.meta.totalPages <= pages.value) return;
 
   pages.value++;
-  const { data: result } = await searchBooks("", pages.value, 5);
+  const { data: result } = await searchBooks("", pages.value, 10);
   books.value?.push(...result.value!.data);
 };
 
@@ -51,5 +56,14 @@ useSeoMeta({
     >
       <MediaBook v-for="i in books" :key="i._id" :item="i" />
     </div>
+    <UIcon
+      class="mx-auto w-full transition-opacity mt-2"
+      :class="{
+        'opacity-100': globalLoading,
+        'opacity-0 duration-500': !globalLoading,
+      }"
+      name="i-svg-spinners-180-ring"
+      dynamic
+    />
   </div>
 </template>
